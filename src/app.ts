0ec6f75756059710c40cbe447efcad79bb833e8c
Paluffel <40839103+Paluffel@users.noreply.github.com>
2020-04-23 14:11:37 +0200

    Update app.ts

--- src/app.ts
+++ src/app.ts
@@ -1,4 +1,3 @@
-/*!
  * Copyright (c) Microsoft Corporation. All rights reserved.
  * Licensed under the MIT License.
  */
@@ -20,63 +19,37 @@ import {
 	Vector3
 } from '@microsoft/mixed-reality-extension-sdk';
 import * as MRE from '@microsoft/mixed-reality-extension-sdk';
+import * as MRESDK from '@microsoft/mixed-reality-extension-sdk';
 
 export default class HelloWorld {
+	public expectedResultDescription = "Different grabbable items.";
+
 	constructor(private context: Context, private baseUrl: string) {
 		this.context.onUserJoined((user) => this.userJoined(user));
-		this.context.onStarted(() => this.started());
 	}
 
 	// Create list to keep track of items attached to users.
 	private attachedItems: {[id: string]: Actor} = {};
 
-	private userJoined(user: User) {
-		// Code to run when a user joins.
-		console.log(`User joined: ${user.name}`);
-
-		// If attached item for user exists, destroy it and remove from list.
-		if (this.attachedItems[user.id]) {
-			this.attachedItems[user.id].destroy();
-			delete this.attachedItems[user.id];
-		}
-	}
-
-	private started() {
-		// Create cube.
-		const mirror = Actor.CreateFromLibrary(this.context, {
-			resourceId: "artifact: 1268209511420658610",
-			actor: {
-				name: 'Mirror',
-				transform: {local: {
-					position: { x: 0, y: 0, z: 0 },
-					scale: { x: 0.5, y: 0.5, z: 0.5}
-				}}
-			}
-		});
-
-		// Create button behavior for cube.
-		mirror.setBehavior(ButtonBehavior).onButton("pressed", (user: User) => {
-			if (!this.attachedItems[user.id]) {
-				// If item for user does not exist, create it and add to list.
-				this.attachedItems[user.id] = Actor.CreateFromLibrary(this.context, {
-					resourceId: "artifact: 1447231871116116890",
-					actor: {
-						name: 'Invert',
-						attachment: {
-							userId: user.id,
-							attachPoint: 'head'
-						},
-						transform: {local: {
-							position: { x: 0, y: 0.065, z: 0.055 },
-							scale: { x: 0.93, y: 0.93, z: 0.93},
-						}}
-					}
-				});
-			} else {
-				// If item already exists, destroy it and delete from list.
-				this.attachedItems[user.id].destroy();
-				delete this.attachedItems[user.id];
-			}
-		});
-	}
+    private userJoined(user: User) {
+        // Code to run when a user joins.
+        console.log(`User joined: ${user.name}`);
+        console.log(user);
+        if (user.name === "Tr4sh P4nda"){
+            Actor.CreateFromLibrary(this.context, {
+                resourceId: "artifact: 1447231871116116890",
+                actor: {
+                    name: 'Specal Paul',
+                    attachment: {
+                        userId: user.id,
+                        attachPoint: 'head'
+                    },
+                    transform: {local: {
+			position: { x: 0, y: 0.065, z: 0.055 },
+			scale: { x: 0.93, y: 0.93, z: 0.93},
+                    }}
+                }
+            });
+        }
+    }
 }
